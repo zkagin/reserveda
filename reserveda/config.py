4c4dd3c00665296a8139b5cbeaf28c985a20aed8
Zach Kagin <zkagin@gmail.com>
2021-10-09 14:37:49 -0700

    fix for postgres uri

--- reserveda/config.py
+++ reserveda/config.py
@@ -3,7 +3,9 @@
 
 import os
 
-SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL").replace(
+    "postgres://", "postgresql://"
+)
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SECRET_KEY = os.getenv("SECRET_KEY")
 
